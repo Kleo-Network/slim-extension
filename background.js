e81a99104e09222f4441b6fa5e546af766ef96ae
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2023-10-15 17:27:20 +0530

    push code

--- background.js
+++ background.js
@@ -1,23 +1,16 @@
-console.log('Background script running!');
+ console.log('Background script running!');
 
-// Data types
-
-type HistoryBatch = {
-    history: chrome.history.HistoryItem[];
-    user_id: string;
-};
-
-// Function to generate a UUID
-function generateUUID(): string {
+// // Function to generate a UUID
+function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        const r = Math.random() * 16 | 0,
-            v = c === 'x' ? r : (r & 0x3 | 0x8);
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
 
 // Function to post data to the API
-async function postToAPI(data: HistoryBatch) {
+async function postToAPI(data) {
     const apiEndpoint = 'http://127.0.0.1:5001/api/v1/core/history/upload';
 
     try {
@@ -38,7 +31,7 @@ async function postToAPI(data: HistoryBatch) {
 }
 
 // Function to store history for a given day
-function storeDayHistory(day: number): void {
+function storeDayHistory(day) {
     const startTime = (new Date().getTime()) - (day * 24 * 60 * 60 * 1000);
     const endTime = startTime + (24 * 60 * 60 * 1000);
 
@@ -47,20 +40,23 @@ function storeDayHistory(day: number): void {
         startTime: startTime,
         endTime: endTime,
         maxResults: 1000
-    },function (results: chrome.history.HistoryItem[]) {
+    }, function (results) {
+        console.log("results?", results);
+        if(results.length > 0){
         chrome.storage.local.get('user_id', function(storageData) {
             if (storageData.user_id) {
                 postToAPI({
-                    history: results.filter(item => item.url && item.title), // Filter out items without url or title
+                    history: results,
                     user_id: storageData.user_id
                 });
             }
         });
+    }
     });
 }
 
 // Function to store all previous history
-function storeAllPreviousHistory(): void {
+function storeAllPreviousHistory() {
     const numberOfDays = 365;
     for (let i = 1; i <= numberOfDays; i++) {
         storeDayHistory(i);
@@ -68,8 +64,9 @@ function storeAllPreviousHistory(): void {
 }
 
 // Listener for when the extension is installed
-chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === "install") {
+chrome.runtime.onInstalled.addListener(function(details) {
+    console.log("details", details);
+    if (details.reason === "install" || details.reason === "update") {
         // Check if user_id is already generated, if not, generate one.
         chrome.storage.local.get('user_id', function(result) {
             if (!result.user_id) {
@@ -84,18 +81,19 @@ chrome.runtime.onInstalled.addListener((details) => {
     }
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' && tab.url) {
         chrome.storage.local.get('user_id', function(storageData) {
             if (storageData.user_id) {
-                const historyData: chrome.history.HistoryItem = {
+                const historyData = {
                     id: tabId.toString(),
                     url: tab.url,
                     title: tab.title || "",
                     lastVisitTime: Date.now(),
-                    typedCount: 0, // These are defaults; you can modify them as needed
+                    typedCount: 0,
                     visitCount: 1
                 };
+                
 
                 postToAPI({
                     history: [historyData],
@@ -106,7 +104,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 });
 
-
-chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.create({ url: "https://app.kleo.network/" });
-});
+// chrome.action.onClicked.addListener(function(tab) {
+//     chrome.tabs.create({ url: "https://app.kleo.network/" });
+// });
--- dist/background.js
+++ dist/background.js
@@ -1,9 +1,104 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 console.log('Background script running!');
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed!');
+// Function to generate a UUID
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+// Function to post data to the API
+function postToAPI(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const apiEndpoint = 'http://127.0.0.1:5001/api/v1/core/history/upload';
+        try {
+            const response = yield fetch(apiEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            const responseData = yield response.json();
+            console.log("Data successfully sent:", JSON.stringify(responseData));
+        }
+        catch (error) {
+            console.error("Error sending data:", error);
+        }
+    });
+}
+// Function to store history for a given day
+function storeDayHistory(day) {
+    const startTime = (new Date().getTime()) - (day * 24 * 60 * 60 * 1000);
+    const endTime = startTime + (24 * 60 * 60 * 1000);
+    chrome.history.search({
+        text: '',
+        startTime: startTime,
+        endTime: endTime,
+        maxResults: 1000
+    }, function (results) {
+        chrome.storage.local.get('user_id', function (storageData) {
+            if (storageData.user_id) {
+                postToAPI({
+                    history: results.filter(item => item.url && item.title),
+                    user_id: storageData.user_id
+                });
+            }
+        });
+    });
+}
+// Function to store all previous history
+function storeAllPreviousHistory() {
+    const numberOfDays = 365;
+    for (let i = 1; i <= numberOfDays; i++) {
+        storeDayHistory(i);
+    }
+}
+// Listener for when the extension is installed
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install") {
+        // Check if user_id is already generated, if not, generate one.
+        chrome.storage.local.get('user_id', function (result) {
+            if (!result.user_id) {
+                const userId = generateUUID();
+                chrome.storage.local.set({ user_id: userId }, function () {
+                    console.log("user_id saved:", userId);
+                });
+            }
+        });
+        storeAllPreviousHistory();
+    }
+});
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url) {
+        chrome.storage.local.get('user_id', function (storageData) {
+            if (storageData.user_id) {
+                const historyData = {
+                    id: tabId.toString(),
+                    url: tab.url,
+                    title: tab.title || "",
+                    lastVisitTime: Date.now(),
+                    typedCount: 0,
+                    visitCount: 1
+                };
+                postToAPI({
+                    history: [historyData],
+                    user_id: storageData.user_id
+                });
+            }
+        });
+    }
 });
 chrome.action.onClicked.addListener((tab) => {
-    console.log('Extension icon clicked in tab:', tab.id);
+    chrome.tabs.create({ url: "https://app.kleo.network/" });
 });
 //# sourceMappingURL=background.js.map
