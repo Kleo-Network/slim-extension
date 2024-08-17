706576219000c0c47b049412c43a608793ccb053
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-17 15:30:06 +0530

    fix: if don't have any thumb then make it dataCard

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -91,11 +91,7 @@ export function updateCardTypeToRenderInAllCards(data: PendingCard[]) {
 	const updatedData = data.map((cardItem, index) => {
 		let cardTypeToRender: CardTypeToRender;
 
-		if (
-			cardItem.urls?.some(
-				(url) => url.url.includes("youtu.be") || url.url.includes("youtube.com")
-			)
-		) {
+		if (cardItem.urls?.some((url) => extractVideoId(url.url))) {
 			cardTypeToRender = CardTypeToRender.YT;
 		} else if (cardItem.cardType === "ImageCard") {
 			cardTypeToRender = CardTypeToRender.IMAGE;
@@ -143,4 +139,17 @@ export const extractThumbNailURL = (videoURL: string) => {
 		? `https://img.youtube.com/vi/${videoId}/default.jpg`
 		: "";
 	return thumbUrl;
+};
+
+// Helper function to extract the video ID from a YouTube URL
+const extractVideoId = (url: string): string | undefined => {
+	let videoId: string | undefined;
+
+	if (url.includes("youtu.be")) {
+		videoId = url.split("youtu.be/")[1]?.split("?")[0];
+	} else if (url.includes("youtube.com/watch")) {
+		videoId = url.split("v=")[1]?.split("&")[0];
+	}
+
+	return videoId;
 };
