aec839a4c0080f3d320104faed489dbab38ce641
princedalsaniya <prince.d@ahduni.edu.in>
2024-09-01 15:23:32 +0530

    refactor: added Activity Chart Label

--- src/HomePageComponent/RadarChartComponent.tsx
+++ src/HomePageComponent/RadarChartComponent.tsx
@@ -31,7 +31,7 @@ const mockLabels = [
 	"Cycling",
 	"Running",
 ];
-const mockValues = [28, 48, 40, 19, 96, 27, 100];
+const mockValues = [35, 70, 55, 80, 60, 45, 90];
 
 export const RadarChartComponent = () => {
 	const chartRef = useRef<any>(null);
@@ -60,18 +60,26 @@ export const RadarChartComponent = () => {
 	}, []);
 
 	return (
-		<div className="h-[270px] w-[367px] rounded-lg flex justify-center items-center bg-primary-btn-100/20">
-			{isLoading && (
-				<div className="w-8 h-8 border-4 border-t-4 border-gray-200 border-t-purple-500 rounded-full animate-spin"></div>
-			)}
-			{!isLoading && (
-				<Radar
-					ref={chartRef}
-					data={radarChartData}
-					options={RadarChartOptions}
-					style={{ height: "100%", width: "100%" }} // Make canvas cover entire div
-				/>
-			)}
+		<div className="h-[270px] w-[367px] rounded-lg flex flex-col justify-between items-center bg-primary-btn-100/20">
+			<div
+				className="h-11 py-3 px-4 text-base font-medium rounded-lg rounded-b-none text-secondary text-center"
+				style={{ width: "inherit" }}
+			>
+				Activity Chart
+			</div>
+			<div className="flex-1 flex justify-center items-center">
+				{isLoading && (
+					<div className="w-8 h-8 border-4 border-t-4 border-gray-200 border-t-purple-500 rounded-full animate-spin"></div>
+				)}
+				{!isLoading && (
+					<Radar
+						ref={chartRef}
+						data={radarChartData}
+						options={RadarChartOptions}
+						style={{ height: "100%", width: "100%" }} // Make canvas cover entire div
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
